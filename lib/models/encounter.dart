771c4aa0670b88cf7b9fe620ee68309f69acaad1
Dalton Craig <dalton.craig1234@gmail.com>
2025-08-06 20:45:52 -0400

    Updates the encounter models

--- lib/models/encounter.dart
+++ lib/models/encounter.dart
@@ -1,5 +1,27 @@
 class Encounter {
-  DateTime encounterDate;
+  final String id;
+  final DateTime creationDate;
+  final DateTime lastModifiedDate;
+  final List<String> personIds;
 
-  Encounter(this.encounterDate);
+  Encounter({
+    required this.id,
+    required this.creationDate,
+    required this.lastModifiedDate,
+    this.personIds = const [],
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "creationDate": creationDate.toIso8601String(),
+      "lastModifiedDate": lastModifiedDate.toIso8601String(),
+      "personIds": personIds,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Encounter(id: $id, creationDate: $creationDate, lastModifiedDate: $lastModifiedDate, personIds: $personIds)';
+  }
 }
